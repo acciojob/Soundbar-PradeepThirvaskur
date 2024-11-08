2307d0447937ca9eb238e99ce956142886187f1e
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-08 08:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+  <script>
+    let currentAudio;
+
+    function playSound(sound) {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+      currentAudio = new Audio(`sounds/${sound}.mp3`);
+      currentAudio.play();
+    }
+
+    function stopSound() {
+      if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+      }
+    }
+  </script>
