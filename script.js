399836d8add92f976475cf8131875bad90676169
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-08 09:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 //your JS code here. If required.
 
-  <script>
+  
     let currentAudio;
 
+function stopSound() {
+    document.getElementById('applause').pause();
+    document.getElementById('applause').currentTime = 0;
 
-    function stopSound() {
-      if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-      }
-    }
-  </script>
+    document.getElementById('boo').pause();
+    document.getElementById('boo').currentTime = 0;
+
+    document.getElementById('gasp').pause();
+    document.getElementById('gasp').currentTime = 0;
+
+    document.getElementById('tada').pause();
+    document.getElementById('tada').currentTime = 0;
+
+    document.getElementById('victory').pause();
+    document.getElementById('victory').currentTime = 0;
+
+    document.getElementById('wrong').pause();
+    document.getElementById('wrong').currentTime = 0;
+  }
+    
