8c67be5b615fb702b502d5c28c0c696648e73969
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-08 09:17:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,6 @@
   <script>
     let currentAudio;
 
-    function playSound(sound) {
-      if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-      }
-      currentAudio = new Audio(`sounds/${sound}.mp3`);
-      currentAudio.play();
-    }
 
     function stopSound() {
       if (currentAudio) {
